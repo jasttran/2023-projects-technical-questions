4981b8d5fd7170f6bc40e57730f8e263e88a9135
Jasmine <jastran1003@gmail.com>
2023-01-07 14:04:31 +1100

    added helper function comments

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -17,58 +17,9 @@ type lassoableAnimal = { type: "pig" | "cow" | "flying_burger", location: locati
 
 // === ADD YOUR CODE BELOW :D ===
 
-// === ExpressJS setup + Server setup ===
-const spaceDatabase = [] as spaceEntity[];
-const app = express();
-app.use(express.json()); // built-in middleware for json
-
-// the POST /entity endpoint adds an entity to your global space database
-app.post('/entity', (req, res) => {
-    // TODO if all properties exist
-    if (req.body.entities === undefined) {
-        res.send(400)
-    }
-    const entities = req.body.entities as spaceEntity[];
-    entities.forEach((entity: spaceEntity) => { spaceDatabase.push(entity) })
-    res.status(200).send({});
-});
-
-// lasooable returns all the space animals a space cowboy can lasso given their name
-app.get('/lassoable', (req, res) => {
-    // [X] not given cowboy name 
-    // [X] cowboy does not exist
-    // [ ] no animals in range 
-
-    // assumes all properties exist due to checking in /entity endpoint
-    if (req.query.cowboy_name === undefined) {
-        res.send(400)
-    }
-    const cowboyEntity = getCowboyEntity(req.query.cowboy_name as string);
-    console.log("cowboy entity")
-    console.log(cowboyEntity)
-    if (cowboyEntity.location.x === Number.POSITIVE_INFINITY) {
-        res.send(400)
-    }
-
-    const lassoLength = (cowboyEntity.metadata as spaceCowboy).lassoLength;
-
-    let lassoableAnimals = [] as lassoableAnimal[]
-    for (const entity of spaceDatabase) {
-        if (isSpaceAnimal(entity.metadata) 
-            && isLassoable(entity.location, cowboyEntity.location, lassoLength)) {
-            lassoableAnimals.push({
-                type: (entity.metadata as spaceAnimal).type, 
-                location: entity.location
-            })
-        }
-    }
-
-    res.status(200).send({space_animals: lassoableAnimals});
-})
-
-function isLassoable(animal: location, cowboy: location, lassoLen: number) {
-    return Math.sqrt(Math.pow(animal.x - cowboy.x, 2) + Math.pow(animal.y - cowboy.y, 2)) <= lassoLen;
-}
+// === HELPER FUNCTIONS ===
+// Helper function that returns the spaceEntity with a name attribute in its metadata
+// equivalent to the given cowboyName.
 function getCowboyEntity(cowboyName: string): spaceEntity {
     for (const entity of spaceDatabase) {
         if ((entity.metadata as spaceCowboy).name === cowboyName) {
@@ -87,8 +38,63 @@ function getCowboyEntity(cowboyName: string): spaceEntity {
     };
 }
 
+// Helper function that determines if the type of the given input is spaceAnimal or spaceCowboy
 function isSpaceAnimal(animal: spaceAnimal | spaceCowboy): animal is spaceAnimal {
     return (animal as spaceAnimal).type !== undefined;
 }
+
+// Helper function that returns all animals lassoable to the given cowboy.
+function getLassoableAnimals(cowboy: spaceEntity, lassoLength: number): lassoableAnimal[]{
+    let lassoableAnimals = [] as lassoableAnimal[]
+    for (const entity of spaceDatabase) {
+        if (isSpaceAnimal(entity.metadata) 
+            && isLassoable(entity.location, cowboy.location, lassoLength)) {
+            lassoableAnimals.push({
+                type: (entity.metadata as spaceAnimal).type, 
+                location: entity.location
+            })
+        }
+    }
+    return lassoableAnimals;
+}
+
+// Helper function that determines if the Pythagorean distance between the cowboy and animal
+// is less than or equal to the given lasso length.
+function isLassoable(animal: location, cowboy: location, lassoLen: number) {
+    return Math.sqrt(Math.pow(animal.x - cowboy.x, 2) + Math.pow(animal.y - cowboy.y, 2)) <= lassoLen;
+}
+
+// === ExpressJS setup + Server setup ===
+const spaceDatabase = [] as spaceEntity[];
+const app = express();
+app.use(express.json()); // built-in middleware for json
+
+// the POST /entity endpoint adds an entity to your global space database
+app.post('/entity', (req, res) => {
+    if (req.body.entities === undefined) {
+        res.send(400)
+    }
+    const entities = req.body.entities as spaceEntity[];
+    entities.forEach((entity: spaceEntity) => { spaceDatabase.push(entity) })
+    res.status(200).send({});
+});
+
+// lasooable returns all the space animals a space cowboy can lasso given their name
+app.get('/lassoable', (req, res) => {
+    if (req.query.cowboy_name === undefined) {
+        res.send(400)
+    }
+    const cowboyEntity = getCowboyEntity(req.query.cowboy_name as string);
+    if (cowboyEntity.location.x === Number.POSITIVE_INFINITY) {
+        res.send(400)
+    }
+
+    const lassoLength = (cowboyEntity.metadata as spaceCowboy).lassoLength;
+    let lassoableAnimals = getLassoableAnimals(cowboyEntity, lassoLength)
+
+    res.status(200).send({space_animals: lassoableAnimals});
+})
+
+
 
 app.listen(8080);
