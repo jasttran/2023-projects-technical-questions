298f6072fae293ed2ac71e944ba3401452c4a8df
Jasmine <jastran1003@gmail.com>
2023-01-07 12:58:42 +1100

    fixed backend

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -20,11 +20,14 @@ type lassoableAnimal = { type: "pig" | "cow" | "flying_burger", location: locati
 // === ExpressJS setup + Server setup ===
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
+app.use(express.json()); // built-in middleware for json
 
 // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    const { entities } = req.body;
-    entities.forEach(entity => { spaceDatabase.push(entity) })
+    if (req.body.entities !== undefined) {
+        const entities = req.body.entities as spaceEntity[];
+        entities.forEach((entity: spaceEntity) => { spaceDatabase.push(entity) })
+    }
     res.status(200).send({});
 });
 
@@ -32,35 +35,7 @@ app.post('/entity', (req, res) => {
 app.get('/lassoable', (req, res) => {
     const { cowboy_name } = req.body;
 
-    // find lassoLength of the cowboy
-    let lassoLen = null;
-    let cowboy: spaceEntity = null;
-    for (const entity of spaceDatabase) {
-        if (entity.type === "space_cowboy" && entity.metadata.name === cowboy_name) {
-            cowboy = entity;
-        }
-    }
-    if (cowboy === null) return res.status(400);
-
-    // find all animals within range
-    let lassoableAnimals = [] as lassoableAnimal[];
-    for (const entity of spaceDatabase) {
-        if (entity.type === "space_animal") {
-            const location = entity.location;
-            const distance = Math.sqrt(Math.pow(location.x - cowboy.location.x, 2) + Math.pow(location.y - cowboy.location.y, 2));
-            if (distance <= lassoLen) {
-                lassoableAnimals.push({
-                    "type": entity.metadata.type,
-                    "location": {
-                        "x": entity.location.x,
-                        "y": entity.location.y
-                    } 
-                })
-            }
-        }
-    }
-
-    res.status(200).send({ space_animals: lassoableAnimals });
+    //res.status(200).send({ space_animals: lassoableAnimals });
 })
 
 app.listen(8080);
